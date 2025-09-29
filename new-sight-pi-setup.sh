2a7dc1edb9d2dbc81ff40dafd25149de27a4190b
Aidan <aidan.cheng94@gmail.com>
2025-09-29 14:33:41 +0100

    Removing @

--- new-sight-pi-setup.sh
+++ new-sight-pi-setup.sh
@@ -145,20 +145,6 @@ server {
         try_files $uri $uri/ @pharmacy;
     }
     
-    # Fallback for client-side routing
-    location @pharmacy {
-        proxy_pass http://127.0.0.1:3001/;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_cache_bypass $http_upgrade;
-        proxy_redirect off;
-    }
-    
     # Default location for other requests
     location / {
         return 200 'nginx is running on new-sight.local - visit /pharmacy for PharmaStock';
